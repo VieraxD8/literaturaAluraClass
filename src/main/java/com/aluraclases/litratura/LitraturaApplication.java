33453b904d07a20bab01fd81c220001ddbeb1db9
Vierax <vieraalfredoj@gmail.com>
2024-05-18 11:24:36 -0500

    nuevo

---
+++ src/main/java/com/aluraclases/litratura/LitraturaApplication.java
@@ -0,0 +1,13 @@
+package com.aluraclases.litratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitraturaApplication.class, args);
+	}
+
+}
